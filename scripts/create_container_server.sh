7ba46026ec5c0c7156d666f913f1228ddd122cb0
Zy1223 <zxu2890@gmail.com>
2025-09-08 22:25:18 +0800

    Update create_container_server.sh

--- scripts/create_container_server.sh
+++ scripts/create_container_server.sh
@@ -6,7 +6,7 @@ docker run -id --name material_detection_server --gpus all \
     --ipc=host \
     --pid=host \
     -e ROS_DOMAIN_ID=99 \
-    -e RMW_IMPLEMENTATION=rmw_zenoh_cpp \
+    -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
     -e NVIDIA_DRIVER_CAPABILITIES=all \
